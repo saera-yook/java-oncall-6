fd387942d480bc5e385ceb9ac49c835b40f6eab6
saera <yook.saera@gmail.com>
2024-12-13 13:59:36 +0900

    feat: 평일과 휴일의 비상 근무 순번을 입력받아라

--- src/main/java/oncall/ApplicationRunner.java
+++ src/main/java/oncall/ApplicationRunner.java
@@ -8,5 +8,7 @@ public class ApplicationRunner {
     public void run() {
         List<String> monthAndDay = InputView.readMonthAndDay();
         TargetMonth targetMonth = new TargetMonth(Integer.parseInt(monthAndDay.get(0)), monthAndDay.get(1));
+        List<String> normalOrderNames = InputView.readNormalOrder();
+        List<String> holidayOrderNames = InputView.readHolidayOrder();
     }
 }
--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -10,4 +10,16 @@ public class InputView {
         String input = Console.readLine();
         return Arrays.stream(input.split(",")).toList();
     }
+
+    public static List<String> readNormalOrder() {
+        System.out.println("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        String input = Console.readLine();
+        return Arrays.stream(input.split(",")).toList();
+    }
+
+    public static List<String> readHolidayOrder() {
+        System.out.println("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        String input = Console.readLine();
+        return Arrays.stream(input.split(",")).toList();
+    }
 }
